bb89a1b62d4b26a0b22d896b0129544979232610
Glitch (giant-checkered-musician) <none>
2023-12-17 06:12:50 +0000

    ✂️😋 Checkpoint ./controllers/convertHandler.js:52675595/6486

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -6,7 +6,7 @@ function ConvertHandler() {
     const match = input.match(regex);
 
     if (match) {
-      const result = parseFloat(match[1]);
+      result = parseFloat(match[1]);
     }
 
     return result;
@@ -19,20 +19,39 @@ function ConvertHandler() {
     const match = input.match(regex);
 
     if (match) {
-      const result = match[2].toLowerCase();
+      result = match[2].toLowerCase();
     }
     return result;
   };
 
   this.getReturnUnit = function (initUnit) {
     let result;
-    
+    let conversionComp = {
+      gal: "L",
+      L: "gal",
+      lbs: "Kg",
+      Kg: "lbs",
+      mi: "Km",
+      Km: "mi",
+    };
+
+    result = conversionComp[initUnit];
     return result;
   };
 
   this.spellOutUnit = function (unit) {
     let result;
 
+    let spellUnit = {
+      gal: "gallons",
+      L: "litres",
+      lbs: "pounds",
+      Kg: "kilograms",
+      mi: "miles",
+      Km: "kilometres",
+    };
+
+    result = spellUnit[unit];
     return result;
   };
 
@@ -40,15 +59,28 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-
-    let conversionComp = {
-      gal: "L",
-      L: "gal",
-      lbs: "Kg",
-
-      Kg: "lbs",
-    };
     let result;
+
+    let returnUnit = this.getReturnUnit(initUnit);
+
+    if (returnUnit == "gal") {
+      result = initNum*(1/galToL)
+    }
+    else if (returnUnit == "L") {
+      result = initNum*(galToL)
+    }
+    else if (returnUnit == "lbs") {
+      result = initNum*(1/lbsToKg)
+    }
+    else  if (returnUnit == "Kg") {
+      result = initNum*(lbsToKg)
+    }
+    else if (returnUnit == "mi") {
+      result = initNum*(1/miToKm)
+    }
+    else if (returnUnit == "Kms") {
+      result = initNum*(miToKm)
+    }
 
     return result;
   };
